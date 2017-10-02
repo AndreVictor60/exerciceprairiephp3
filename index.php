e2dc4393c5ed28efb93dea7a63c76fe267bb8ff8
AndreVictor60 <andre.victor60@free.fr>
2017-10-02 10:56:21 +0200

    Modification de l'exercice

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 echo "Les nombres impairs jusqu'a 300 sont : <br />";
-// Boucle FOR $i = 0, $i jusqu'a 300 et chaque tour, on n'ajoute +1
+// Boucle FOR $i = 1, $i jusqu'a 300 et chaque tour, on n'ajoute +2 à $i
 for($i = 1; $i < 300;$i=$i+2)
 {
-  // 1 + 1 = 2, 1 + 2 =3 ...
   echo $i."<br />";
 }
 ?>
