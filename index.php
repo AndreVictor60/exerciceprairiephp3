d1f2d2699cc37f01d8beba1f60b1acb7a3bebd5c
AndreVictor60 <andre.victor60@free.fr>
2017-10-02 15:20:29 +0200

    Correction de l'exercice 3 php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 echo 'Les nombres impairs jusqu\'a 300 sont : <br />';
 // Boucle FOR $i = 0, $i jusqu'a 300 et chaque tour, on n'ajoute +1
-for($i = 1; $i < 300;$i=$i+2)
+for($i = 0; $i < 300;$i++)
 {
+  $i++;
   echo $i."<br />";
+}
+// Boucle WHILE $number = 0, tant que $number < 300. Si $number modulo 2 différent de 0 il est impairs et on ajouter +1
+$number=0
+while($number < 300){
+  if($number % 2 != 0){
+    echo $number . ' ';
+  }
+  $number++;
 }
 ?>
